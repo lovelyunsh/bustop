9aa76f7f813510a4f5127b971296a52185c6e489
so0ony <audtnswh4221@gmail.com>
2019-10-10 11:36:46 +0900

    Sinup.xml Button Click Log Respond Function

--- Bustop_User/app/src/main/java/org/soonytown/bustop_user/SignUpActivity.java
+++ Bustop_User/app/src/main/java/org/soonytown/bustop_user/SignUpActivity.java
@@ -1,6 +1,8 @@
 package org.soonytown.bustop_user;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,19 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.signUpButton:
+                    Log.e("클릭", "클릭");
+                    break;
+            }
+        }
+    };
 }
+
