7895f52ffdef202a00146f7d0125c39c478c111a
so0ony <audtnswh4221@gmail.com>
2019-10-14 14:03:07 +0900

    Login Function Complete

--- Bustop_User/app/src/main/java/org/soonytown/bustop_user/LoginActivity.java
+++ Bustop_User/app/src/main/java/org/soonytown/bustop_user/LoginActivity.java
@@ -55,25 +55,22 @@ public class LoginActivity extends AppCompatActivity {
         String passwordCheck = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
 
         if(email.length() > 0 && password.length() > 0) {
-            if(password.equals(passwordCheck)) {
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    startToast("회원가입에 성공하였습니다.");
-                                }
-                                else {
-                                    if(task.getException() != null) {
-                                        startToast(task.getException().toString());
-                                    }
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startToast("로그에 성공하였습니다.");
+                            } else {
+                                if(task.getException() != null) {
+                                    startToast(task.getException().toString());
                                 }
                             }
-                        });
-            } else {
-                startToast("비밀번호가 일치하지 않습니다.");
-            }
+
+                            // ...
+                        }
+                    });
         } else {
             startToast("이메일 또는 비밀번호를 입력해 주세요.");
         }
