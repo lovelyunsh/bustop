2636f09b6af028cc321acceb478e8fa3489458ef
so0ony <audtnswh4221@gmail.com>
2019-10-28 19:00:22 +0900

    Login Image Edit

--- Bustop_User/app/src/main/java/org/soonytown/bustop_user/MainActivity.java
+++ Bustop_User/app/src/main/java/org/soonytown/bustop_user/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }
 //    };
-//
+
     private void startSignUpActivity() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
