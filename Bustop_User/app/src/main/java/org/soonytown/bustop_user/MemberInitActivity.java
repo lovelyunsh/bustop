6f83925331dc877f686f9b7eec3081d6b3f8c8c3
so0ony <audtnswh4221@gmail.com>
2019-11-05 19:46:49 +0900

    Member Info - 1

--- Bustop_User/app/src/main/java/org/soonytown/bustop_user/MemberInitActivity.java
+++ Bustop_User/app/src/main/java/org/soonytown/bustop_user/MemberInitActivity.java
@@ -1,26 +1,28 @@
 package org.soonytown.bustop_user;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MemberInitActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+    private static final String TAG = "MemberInitActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_init);
 
-        mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.checkButton).setOnClickListener(onClickListener);
     }
@@ -45,6 +47,26 @@ public class MemberInitActivity extends AppCompatActivity {
         if(name.length() > 0 && phoneNumber.length() > 9 && birthDay.length() > 5 && address.length() > 0) {
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            MemberInfo memberInfo = new MemberInfo(name, phoneNumber, birthDay, address);
+
+            if (user == null) {
+                db.collection("users").document(user.getUid()).set(memberInfo)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                startToast("회원정보 등록을 성공하였습니다.");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                startToast("회원정보 등록에 실패하였습니다.");
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+            }
 
 
         } else {
